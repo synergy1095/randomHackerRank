7768aa9e2cdad8181a872f655a294f878931de98
synergy1095 <synergy1095@gmail.com>
2020-01-30 00:21:49 -0800

    meh

--- sockMerchant/SockMerchant.js
+++ sockMerchant/SockMerchant.js
@@ -1,72 +1,86 @@
-"use strict";
+// "use strict";
 
-const fs = require("fs");
+// const fs = require("fs");
 
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
+// process.stdin.resume();
+// process.stdin.setEncoding("utf-8");
 
-let inputString = "";
-let currentLine = 0;
+// let inputString = "";
+// let currentLine = 0;
 
-process.stdin.on("data", inputStdin => {
-  inputString += inputStdin;
-});
+// process.stdin.on("data", inputStdin => {
+//   inputString += inputStdin;
+// });
 
-process.stdin.on("end", _ => {
-  inputString = inputString
-    .replace(/\s*$/, "")
-    .split("\n")
-    .map(str => str.replace(/\s*$/, ""));
+// process.stdin.on("end", _ => {
+//   inputString = inputString
+//     .replace(/\s*$/, "")
+//     .split("\n")
+//     .map(str => str.replace(/\s*$/, ""));
 
-  main();
-});
+//   main();
+// });
 
-function readLine() {
-  return inputString[currentLine++];
-}
+// function readLine() {
+//   return inputString[currentLine++];
+// }
 
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar = []) {
-    if (ar == null || ar.length == 0) {
-        console.log("empty pile of socks");
-        return null;
-    } else if ((n < 1) || (n > 100) || (ar.length > n)) {
-        console.log("outside constraints");
-        return null;
-    } 
-    for (let i = 0; i < n; i++) {
-        if ((ar[i] > 100) || (ar[i] < 1)) {
-            console.log("outside constraints");
-            return null;
-        }
+  if (ar == null || ar.length == 0) {
+    console.log("empty pile of socks");
+    return null;
+  } else if (n < 1 || n > 100 || ar.length > n) {
+    console.log("outside constraints");
+    return null;
+  }
+  for (let i = 0; i < n; i++) {
+    if (ar[i] > 100 || ar[i] < 1) {
+      console.log("outside constraints");
+      return null;
     }
+  }
 
-    let sockTypeCount = {};
-    let pairCount = 0;
-    for (let j = 0; j < n; j++) {
-        if (sockTypeCount.hasOwnProperty(ar[j])) {
-            sockTypeCount[ar[j]]++;
-            if (sockTypeCount[ar[j]] % 2 == 0) pairCount++;
-        } else {
-            sockTypeCount[ar[j]] = 1;
-        }
+  let sockTypeCount = {};
+  let pairCount = 0;
+  for (let j = 0; j < n; j++) {
+    if (sockTypeCount.hasOwnProperty(ar[j])) {
+      sockTypeCount[ar[j]]++;
+      if (sockTypeCount[ar[j]] % 2 == 0) pairCount++;
+    } else {
+      sockTypeCount[ar[j]] = 1;
     }
+  }
 
-    return pairCount;
+  return pairCount;
 }
 
 function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  //   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-  const n = parseInt(readLine(), 10);
+  //   const n = parseInt(readLine(), 10);
 
-  const ar = readLine()
-    .split(" ")
-    .map(arTemp => parseInt(arTemp, 10));
+  //   const ar = readLine()
+  //     .split(" ")
+  //     .map(arTemp => parseInt(arTemp, 10));
+  let arr;
+  (arr = []).length = 20;
+  arr.fill(0);
+  console.log(arr);
+
+  let n = 9, ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
   let result = sockMerchant(n, ar);
 
-  ws.write(result + "\n");
+  console.log('\n' + result);
 
-  ws.end();
+  n = 10, ar = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3];
+  result = sockMerchant(n, ar);
+
+  console.log("\n" + result);
+  //   ws.write(result + "\n");
+
+  //   ws.end();
 }
+
+main();
